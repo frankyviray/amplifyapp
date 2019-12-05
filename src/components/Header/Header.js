a5ba3dccc6d80c428b19eeda00e39162674b7127
Franky Viray <harrypotterfan6969@yahoo.com>
2019-12-05 10:00:34 -0800

    asdf

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
 
+
 const Header = props => {
 
     return (
         <div id="nav-wrapper">
             <nav className="navbar">
-                <a className="navbar-brand">
+                {/* <a className="navbar-brand"> */}
                     <img src="assets/img/ace_logo.png" width="400px" height="" alt="" />
-                    <form>
-                        <a href="https://www.acetools.com/">Ace Industrial Supply, 7535 San Fernando Road, Burbank, CA</a>
+                    {/* <form>
+                        <a href="https://www.acetools.com/">Home</a>
                     </form>
+                    <form>
+                        <a href="https://www.acetoolscatalog.com/">Catalog</a>
+                    </form>
+                    <form>
+                        <a href="https://www.aceindustrialtools.com/">Tools By Trade</a>
+                    </form> */}
                 
-                </a>
+                {/* </a> */}
             </nav>
         </div>
     )
